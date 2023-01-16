fdc5a9cca73a097fe47dd650dc17449815f4ff2b
pb-uk <23248021+pb-uk@users.noreply.github.com>
2023-01-16 00:19:45 +0000

    Fetching station

--- src/flood-monitoring-api/index.ts
+++ src/flood-monitoring-api/index.ts
@@ -1,10 +1,28 @@
-const baseUrl = 'https://environment.data.gov.uk/flood-monitoring';
+import type { StationInterface } from './station';
 
-const fetchApi = async (path: string) => {
-  const response = await fetch(`${baseUrl}${path}`);
-  return response.json();
+type ApiResponseItem = Record<string, unknown>;
+
+type ApiResponse2 = {
+  data: Record<string, unknown>;
+  response: { data: Record<string, unknown>; response: Response };
 };
 
-export const fetchStation = (id: string) => {
-  return fetchApi(`/id/stations/${id}`);
-}
+type ApiResponse = {
+  data: Record<string, unknown>;
+  response: Response;
+};
+
+const baseUrl = 'https://environment.data.gov.uk/flood-monitoring';
+
+const fetchApi = async (path: string): Promise<ApiResponse> => {
+  const response = await fetch(`${baseUrl}${path}`);
+  const data = await response.json();
+  return { data, response };
+};
+
+export const fetchStation = async (
+  id: string
+): Promise<[StationInterface, ApiResponse]> => {
+  const response = await fetchApi(`/id/stations/${id}`);
+  return [response.data.items as unknown as StationInterface, response];
+};
--- src/station/station.store.ts
+++ src/station/station.store.ts
@@ -1,9 +1,24 @@
 import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
 
+import { fetchStation as apiFetchStation } from '@/flood-monitoring-api';
+
+import type { ApiResponse } from '@/flood-monitoring-api';
+
+type StationResponse = [item: Station | null, response: ApiResponse];
+
+export type Reading = [date: Date, value: number];
+
+export interface Measure {
+  notation: string;
+  latestReading?: Reading;
+  measures: Measure[];
+}
+
 export interface Station {
   id: string;
   name: string;
+  // measures: Measure[];
 }
 
 export const useStationStore = defineStore('station', () => {
@@ -13,8 +28,13 @@ export const useStationStore = defineStore('station', () => {
     return stations.value[id];
   });
 
-  const fetchStation = async (id: string): Promise<Station | undefined> => {
-    return { id, name: 'Mock station ' + new Date().toISOString()};
+  const fetchStation = async (id: string): Promise<StationResponse> => {
+    const [fetched, response] = await apiFetchStation(id);
+    const station: Station = {
+      id: fetched.stationReference,
+      name: fetched.label,
+    };
+    return [station, response];
   };
 
   return { fetchStation };
