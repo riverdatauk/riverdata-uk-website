eb9909f41aaa7f32fb0c1d31075464df03bd30c2
pb-uk <23248021+pb-uk@users.noreply.github.com>
2023-08-09 18:48:45 +0100

    Add stations list

---
+++ site/data/stationsList.js
@@ -0,0 +1,10 @@
+const stations = require('./stations.json');
+
+const byType = {};
+
+Object.values(stations).forEach((station) => {});
+
+module.exports = {
+  list: 23,
+  stationsList: Object.values(stations),
+};
